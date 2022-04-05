5504d0d63e8b8535ac988300ccb2987913037d20
MANJREKAR <103055899+MANJREKAR@users.noreply.github.com>
2022-04-05 21:55:29 +0530

    Create Program-2.java

---
+++ Program-2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Odd
+{
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in):
+    System.out.println("Enter the integer value A: ");
+    int a=sc.nextInt();
+    
