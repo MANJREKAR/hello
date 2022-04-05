b3290dd29a70bd5ae6eb55a200a0f5a36c8bb7cc
MANJREKAR <103055899+MANJREKAR@users.noreply.github.com>
2022-04-05 21:32:23 +0530

    Create Program-2.java

---
+++ Program-2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Integer
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the Interger A :");
+int a=sc.nextInt();
+for(int i=1;i<=n;i++)
+{
+if(i%2==1)
+System.out.println(i+" ");
+}
+}
+}
