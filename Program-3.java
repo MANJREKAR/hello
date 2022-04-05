5a7e6b6f6e0ae3f4666ea6b02bd7ac8ae7fabe95
MANJREKAR <103055899+MANJREKAR@users.noreply.github.com>
2022-04-05 21:36:07 +0530

    Update and rename Program-2.java to Program-3.java

--- Program-3.java
+++ Program-3.java
@@ -6,10 +6,10 @@ public static void main(String[] args)
 Scanner sc=new Scanner(System.in);
 System.out.println("Enter the Interger A :");
 int a=sc.nextInt();
-for(int i=1;i<=n;i++)
+for(int i=1;i<=a;i++)
 {
 if(i%2==1)
-System.out.println(i+" ");
+System.out.print(i+" ");
 }
 }
 }
