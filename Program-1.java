19827e8aa97dbc1e3ae0b036761783a2b425743a
MANJREKAR <103055899+MANJREKAR@users.noreply.github.com>
2022-04-05 21:08:26 +0530

    Create Program-1.java

---
+++ Program-1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Calculator
+{
+  public static void main(String[] args)
+  {
+  Scanner sc=new Scanner(System.in);
+  System.out.println("Enter the value of A : ");
+  double a=sc.nextDouble();
+  System.out.println("Enter the value of B : ");
+  double b=sc.nextDouble();
+  double add=a+b;
+  double Sub=a+b;
+  double Mul=a+b;
+  double Div=a+b;
+  System.out.println("Addition is : " +add);
+  System.out.println("Subraction is : " +add);
+  System.out.println("Multiplication is : " +add);
+  System.out.println("Division is : " +add);
+}
+}
+  
+  
