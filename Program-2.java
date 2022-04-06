bc4c0e41fe43b044114acd8f594e8ae440898b2c
MANJREKAR <103055899+MANJREKAR@users.noreply.github.com>
2022-04-06 19:12:34 +0530

    Update Program-2.java

--- Program-2.java
+++ Program-2.java
@@ -3,7 +3,17 @@ public class Odd
 {
   public static void main(String[] args)
   {
-    Scanner sc=new Scanner(System.in):
+    Scanner sc=new Scanner(System.in);
     System.out.println("Enter the integer value A: ");
     int a=sc.nextInt();
+    for(int i=1;i<a;i++)
+    {
+      if(i%2==1)
+        System.out.println(i+" ");
+    }
+  }
+    
+    
+   
+    
     
